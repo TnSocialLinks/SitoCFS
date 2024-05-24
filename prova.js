ca4e00967f0a1e19040bc52bd2bff1fa87fa7a46
TnSocialLinks <tnsocialinks@gmail.com>
2024-05-24 19:11:25 +0200

    modifiche

--- prova.js
+++ prova.js
@@ -3,18 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const url = 'https://backendcfs.vercel.app/api/ultime-offerte'; 
 
     // Effettua la richiesta GET al backend
-    axios.get(url)
-        .then(response => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
             // Ottieni i nomi delle cartelle dalla risposta
-            const folderNames = response.data.folders;
+            const folderNames = data.folders;
             
             const offerta1 = document.getElementById('offerta1');
             const offerta2 = document.getElementById('offerta2');
             const offerta3 = document.getElementById('offerta3');
 
-            if (folderNames.length > 0) folder1.textContent = 'Folder 1: ' + folderNames[0];
-            if (folderNames.length > 1) folder2.textContent = 'Folder 2: ' + folderNames[1];
-            if (folderNames.length > 2) folder3.textContent = 'Folder 3: ' + folderNames[2];
+            if (folderNames.length > 0) offerta1.textContent = folderNames[0];
+            if (folderNames.length > 1) offerta2.textContent = folderNames[1];
+            if (folderNames.length > 2) offerta3.textContent = folderNames[2];
 
 
 
