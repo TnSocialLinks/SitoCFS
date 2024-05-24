878093b957831ee28c77e0aab862f83423c4921c
TnSocialLinks <tnsocialinks@gmail.com>
2024-05-24 20:36:07 +0200

    modifiche

--- prova.js
+++ prova.js
@@ -6,25 +6,23 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            // Ottieni i nomi delle cartelle e le immagini dalla risposta
+            // Ottieni i nomi delle cartelle e gli URL delle immagini dalla risposta
             const folders = data.folders;
-
             const offerta1 = document.getElementById('offerta1');
             const offerta2 = document.getElementById('offerta2');
             const offerta3 = document.getElementById('offerta3');
-
             // Assegna le immagini ai tag <img> esistenti
-            if (folders.length > 0) {            
-                document.getElementById('img1').src = getImagePath(folders[0].name, folders[0].significantImage);
-                offerta1.textContent = folderNames[0];
+            if (folders.length > 0) {
+                document.getElementById('img1').src = folders[0].imageUrl;
+                offerta1.textContent = folders[0].name;
             }
             if (folders.length > 1) {
-                document.getElementById('img2').src = getImagePath(folders[1].name, folders[1].significantImage);
-                offerta2.textContent = folderNames[1];
+                document.getElementById('img2').src = folders[1].imageUrl;
+                offerta2.textContent = folders[1].name;
             }
             if (folders.length > 2) {
-                document.getElementById('img3').src = getImagePath(folders[2].name, folders[2].significantImage);
-                offerta3.textContent = folderNames[2];
+                document.getElementById('img3').src = folders[2].imageUrl;
+                offerta3.textContent = folders[2].name;
             }
         })
         .catch(error => {
@@ -32,8 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 });
 
-// Funzione per ottenere il percorso completo dell'immagine
-function getImagePath(folderName, imageName) {
-    return `https://backendcfs.vercel.app/progetti/${folderName}/${imageName}`;
-}
+
+
+
 
