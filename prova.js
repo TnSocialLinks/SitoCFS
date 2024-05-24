83fb0515c7b6ac918627900f6f960f4f4636b6d2
TnSocialLinks <tnsocialinks@gmail.com>
2024-05-24 15:36:28 +0200

    Modifiche

--- prova.js
+++ prova.js
@@ -1,7 +1,9 @@
-fetch("https://backendcfs.vercel.app/api/users")
-    .then(response => response.text())
+fetch("https://backendcfs.vercel.app/api/greet")
+    .then(response => response.json())
     .then(data => {
-        console.log('Dati ricevuti dal server locale:', data);
+        // Aggiungi i dati al testo di un elemento HTML con id "dati"
+        document.getElementById("dato").innerText = data.message;
+        document.getElementById("dati").innerText = data.response;
     })
     .catch(error => {
         console.error('Errore durante il recupero dei dati dal server locale:', error);
